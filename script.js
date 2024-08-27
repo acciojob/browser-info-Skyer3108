d6e3f5c34f2635cf3d423433d85de5da6724400d
Skyer3108 <136808389+Skyer3108@users.noreply.github.com>
2024-08-27 15:51:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
-//your JS code here. If required.
+
+
+
+function detectBrowser() {
+   
+    const userAgent = navigator.userAgent;
+
+   
+    let browserName = "Unknown";
+    let version = "Unknown";
+
+    
+    if (userAgent.indexOf("Chrome") > -1) {
+        browserName = "Chrome";
+        version = userAgent.match(/Chrome\/(\d+)/)[1];
+    } else if (userAgent.indexOf("Firefox") > -1) {
+        browserName = "Firefox";
+        version = userAgent.match(/Firefox\/(\d+)/)[1];
+    } else if (userAgent.indexOf("Safari") > -1) {
+        browserName = "Safari";
+        version = userAgent.match(/Version\/(\d+)/)[1];
+    } else if (userAgent.indexOf("Edge") > -1) {
+        browserName = "Edge";
+        version = userAgent.match(/Edge\/(\d+)/)[1];
+    } else if (userAgent.indexOf("MSIE") > -1 || !!document.documentMode) {
+        browserName = "Internet Explorer";
+        version = userAgent.match(/MSIE (\d+)/)[1];
+    }
+
+  
+    document.getElementById('browser-info').textContent = `You are using ${browserName} version ${version}`;
+}
+
+
+detectBrowser();
